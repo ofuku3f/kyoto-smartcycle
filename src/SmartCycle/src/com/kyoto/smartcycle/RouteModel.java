f25f7cd20f554683891be7fde1ae40d354b15821
Toru Uenoyama <t.uenoyama@gmail.com>
2012-02-19 16:59:54 +0900

    ルート解析作成完了

--- src/SmartCycle/src/com/kyoto/smartcycle/RouteModel.java
+++ src/SmartCycle/src/com/kyoto/smartcycle/RouteModel.java
@@ -1,5 +1,7 @@
 package com.kyoto.smartcycle;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,9 +15,33 @@ public class RouteModel {
 	public static RouteModel ParseJSON(String json) throws JSONException {
 		RouteModel route = new RouteModel();
 		
+		ArrayList<Step> localSteps = new ArrayList<RouteModel.Step>();
+		
 		// JSONをパース
 		JSONObject obj = new JSONObject(json);
-		JSONArray routelist = obj.getJSONArray("route");
+		JSONArray routelist = obj.getJSONArray("routes");
+		JSONObject mainroute = routelist.getJSONObject(0);
+		JSONArray legslist = mainroute.getJSONArray("legs");
+		for(int i = 0; i < legslist.length(); i++){
+			JSONArray steps = legslist.getJSONObject(i).getJSONArray("steps");
+			for(int j = 0; j < steps.length(); j++){
+				Step s = route.new Step();
+				JSONObject step = steps.getJSONObject(i);
+				s.distance = step.getJSONObject("distance").getInt("value");
+				s.duration = step.getJSONObject("duration").getInt("value");
+				JSONObject st = step.getJSONObject("start_location");
+				s.start_addr = new GeoPoint((int)(st.getDouble("lat") * 1E6), (int)(st.getDouble("lng")));
+				JSONObject en = step.getJSONObject("end_location");
+				s.end_addr = new GeoPoint((int)(en.getDouble("lat") * 1E6), (int)(en.getDouble("lng")));
+				s.instruction = step.getString("html_instructions");
+				localSteps.add(s);
+			}
+		}
+		
+		route.steps = new Step[localSteps.size()];
+		for(int i = 0; i < localSteps.size(); i++){
+			route.steps[i] = localSteps.get(i);
+		}
 		
 		
 

f1519f9ee969d6f2c32b8b5c1b193a7eb9d02574
Toru Uenoyama <t.uenoyama@gmail.com>
2012-02-19 12:53:00 +0900

    コミット２

--- src/SmartCycle/gen/com/sonyericsson/extras/liveware/sdk/R.java
+++ src/SmartCycle/gen/com/sonyericsson/extras/liveware/sdk/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.kyoto.smartcycle;
+package com.sonyericsson.extras.liveware.sdk;
 
 public final class R {
     public static final class attr {
